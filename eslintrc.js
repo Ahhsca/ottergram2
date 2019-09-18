bb4c7715ecb3b16b35ea350efb2df963922dd52c
Oscar Cheung <31262167+Ahhsca@users.noreply.github.com>
2019-09-17 17:56:44 -0700

    Add files via upload

---
+++ eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	"env": {
+		"browser": true
+	},
+	"entends": "eslint:recommended", "rules": {
+		"indent": [
+		"error",
+		2
+		],
+		"linebreak-style": [
+		"error",
+		"unix"
+		],
+		"quotes": [
+		"error",
+		"double"
+		],
+		"semi": [
+		"error",
+		"always"
+		]
+	}
+};
